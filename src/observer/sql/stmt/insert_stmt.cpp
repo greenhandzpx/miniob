2f4f76f877903ed02698c847ef4829712365a953
shootfirst <2782457525@qq.com>
2022-10-20 10:31:27 +0800

    :feat: finish typecast

--- src/observer/sql/stmt/insert_stmt.cpp
+++ src/observer/sql/stmt/insert_stmt.cpp
@@ -70,9 +70,74 @@ RC InsertStmt::create(Db *db, const Inserts &inserts, Stmt *&stmt)
       const AttrType field_type = field_meta->type();
       const AttrType value_type = tuples[k][i].type;
       if (field_type != value_type) { // TODO try to convert the value type to field type
-        LOG_WARN("field type mismatch. table=%s, field=%s, field type=%d, value_type=%d", 
+        // *********************************typecast****************************************
+        if (field_type == INTS && value_type == CHARS) {
+          float num = 0;
+          std::string str = static_cast<char*>(tuples[k][i].data);
+          string2float(str, &num);
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = INTS;
+          int* data = static_cast<int*>(tuples[k][i].data);
+
+          if (num - (float)((int)num) >= 0.5) num += 1;
+          *data = (int)num;
+          
+        } else if (field_type == FLOATS && value_type == CHARS) {
+          float num = 0;
+          std::string str = static_cast<char*>(tuples[k][i].data);
+          string2float(str, &num);
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = FLOATS;
+          float* data = static_cast<float*>(tuples[k][i].data);
+          *data = num;
+          
+        } else if (field_type == FLOATS && value_type == INTS) {
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = FLOATS;
+          int* data = static_cast<int*>(tuples[k][i].data);
+          float value = (float)*data; 
+          float *float_data = static_cast<float*>(tuples[k][i].data);
+          *float_data = value;
+        } else if (field_type == INTS && value_type == FLOATS) {
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = INTS;
+          float* data = static_cast<float*>(tuples[k][i].data);
+          int value = (int)*data;
+          if (*data - (float)value >= 0.5) value += 1;
+          int *int_data = static_cast<int*>(tuples[k][i].data);
+          *int_data = value;
+        
+          
+        } else if (field_type == CHARS && value_type == FLOATS) {
+          char* str = static_cast<char*>(tuples[k][i].data);
+          float* data = static_cast<float*>(tuples[k][i].data);
+          char* s = const_cast<char*>(double2string(*data).data());
+          int idx = 0;
+          for (; s[idx] != '\0'; idx++) {
+            str[idx] = s[idx];
+          }
+          str[idx] = '\0';
+
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = CHARS;
+          
+        } else if (field_type == CHARS && value_type == INTS) {
+          char* str = static_cast<char*>(tuples[k][i].data);
+          int* data = static_cast<int*>(tuples[k][i].data);
+          char* s = const_cast<char*>(std::to_string(*data).data());
+          for ( int i = 0; s[i] != '\0'; i++) {
+            str[i] = s[i];
+          }
+          AttrType* att = const_cast<AttrType*>(&tuples[k][i].type);
+          *att = CHARS;
+
+        //  ******************************typecast*********************************
+        } else {
+        printf("bad\n");
+          LOG_WARN("field type mismatch. table=%s, field=%s, field type=%d, value_type=%d", 
                 table_name, field_meta->name(), field_type, value_type);
-        return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+          return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+        }
       }
     }
 
