19de7578f56618cc3e2cef41e8671976aa8b912f
greenhandzpx <893522573@qq.com>
2022-11-03 10:48:52 +0800

    update-select: fix typecast

--- src/observer/sql/operator/update_operator.cpp
+++ src/observer/sql/operator/update_operator.cpp
@@ -8,6 +8,7 @@
 
 //***************************************************************upelect************************************************************
 #include "util/util.h"
+#include <string>
 
 RC UpdateOperator::open()
 {
@@ -47,17 +48,79 @@ RC UpdateOperator::open()
         return RC::SCHEMA_FIELD_NOT_EXIST;
       }
 
-      const Value &value = update_stmt_->values()[i];
-      if (field->type() != value.type && value.type != AttrType::NULLS) {
-        LOG_ERROR("Invalid value type. table name =%s, field name=%s, type=%d, but given=%d",
-            table_meta.name(),
-            field->name(),
-            field->type(),
-            value.type);
-        return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+      const Value *value = &update_stmt_->values()[i];
+
+      if (field->type() != value->type && value->type != AttrType::NULLS) {
+
+        // ******************************typecast*********************************
+        if (value->type == AttrType::CHARS && field->type() == AttrType::INTS) {
+          float num = 0;
+          Value* v = const_cast<Value*>(value);
+          std::string str = static_cast<char*>(v->data);
+          string2float(str, &num);
+          if (num - (float)((int)num) >= 0.5) num += 1;
+          v->type = AttrType::INTS;
+          *((int*)(v->data)) = (int)num;
+        } else if (value->type == AttrType::CHARS && field->type() == AttrType::FLOATS) {
+          float num = 0;
+          Value* v = const_cast<Value*>(value);
+          std::string str = static_cast<char*>(v->data);
+          string2float(str, &num);
+          v->type = AttrType::FLOATS;
+          *((float*)(v->data)) = num;
+        } else if (value->type == AttrType::INTS && field->type() == AttrType::FLOATS) {
+          Value* v = const_cast<Value*>(value);
+          int* num = static_cast<int*>(v->data);
+          float n = (float)*num;
+          float* fnum = static_cast<float*>(v->data);
+          *fnum = n;
+          v->type = AttrType::FLOATS;
+        } else if (value->type == AttrType::INTS && field->type() == AttrType::CHARS) {
+          Value* v = const_cast<Value*>(value);
+          int* num = static_cast<int*>(v->data);
+          char* str = static_cast<char*>(v->data);
+
+          std::string num_str = std::to_string(*num);
+          char* s = const_cast<char*>(num_str.data());
+          // char* s = const_cast<char*>(std::to_string(*num).data());
+          for ( int i = 0; s[i] != '\0'; i++) {
+            str[i] = s[i];
+          }
+          v->type = CHARS;
+
+        } else if (value->type == AttrType::FLOATS && field->type() == AttrType::INTS) {
+          Value* v = const_cast<Value*>(value);
+          float* num = static_cast<float*>(v->data);
+          int n = (int)*num;
+          if (*num - (float)n >= 0.5) n += 1;
+          int* inum = static_cast<int*>(v->data);
+          *inum = n;
+          v->type = AttrType::INTS;
+
+        } else if (value->type == AttrType::FLOATS && field->type() == AttrType::CHARS) {
+          Value* v = const_cast<Value*>(value);
+          float* num = static_cast<float*>(v->data);
+          char* str = static_cast<char*>(v->data);
+
+          char* s = const_cast<char*>(double2string(*num).data());
+          for ( int i = 0; s[i] != '\0'; i++) {
+            str[i] = s[i];
+          }
+          v->type = CHARS;
+
+        // **********************************typecast******************************************
+        } else {
+          LOG_ERROR("Invalid value type. table name =%s, field name=%s, type=%d, but given=%d",
+              table_meta.name(),
+              field->name(),
+              field->type(),
+              value->type);
+          return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+        }
       }
 
-      if (!field->nullable() && value.type == AttrType::NULLS) {
+
+      if (!field->nullable() && value->type == AttrType::NULLS) {
         return RC::NULL_OP_ON_NON_NULLABLE;
       }
 
@@ -70,25 +133,25 @@ RC UpdateOperator::open()
         // if (strcmp((const char*)value.data, "N01") == 0|| strcmp((const char*)value.data, "n01") == 0) {
         //   strcpy((char *)value.data, "ddd");
         // }
-        const size_t data_len = strlen((const char *)value.data);
+        const size_t data_len = strlen((const char *)value->data);
         if (copy_len > data_len) {
           copy_len = data_len + 1;
         }
       }
 
-      if (value.type == AttrType::NULLS) {
+      if (value->type == AttrType::NULLS) {
         if (!field->nullable()) {
           LOG_WARN("shouldn't set a field a NULL value since the field isn't nullable");
           return RC::SCHEMA_FIELD_TYPE_MISMATCH;
         }
       } else {
         // modify the field in the record data
-        memcpy(record.data() + field->offset(), value.data, copy_len);
+        memcpy(record.data() + field->offset(), value->data, copy_len);
       }
 
       // set null
       int null_bit = 1 << table_meta.get_field_place(field->name());
-      if (field->nullable() && value.type == AttrType::NULLS) {
+      if (field->nullable() && value->type == AttrType::NULLS) {
         *(int *)(record.data() + NULLMAP_OFFSET) |= null_bit;
       } else {
         *(int *)(record.data() + NULLMAP_OFFSET) &= ~null_bit;
